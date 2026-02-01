d1306d17d4dc65bd9dae7f1e6073a5c8a8997fbf
Maksim Zayats <maximzayats1@gmail.com>
2026-02-01 08:28:16 +0400

    docs: update pencil icon in pyodide-runner to use edit icon

--- docs/sphinx-pyodide-runner/sphinx_pyodide_runner/_static/pyodide-runner.js
+++ docs/sphinx-pyodide-runner/sphinx_pyodide_runner/_static/pyodide-runner.js
@@ -30,7 +30,7 @@ await micropip.install([${escaped}])
 
   // Tabler icons — same SVG structure as sphinx-copybutton's icon
   const playSvg = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-player-play" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 4v16l13-8z"/></svg>`;
-  const pencilSvg = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5-10.5a2.121 2.121 0 0 0-3-3l-10.5 10.5v4"/><path d="M13.5 6.5l3 3"/></svg>`;
+  const pencilSvg = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-1"/><path d="M20.385 6.585a2.1 2.1 0 0 0-2.97-2.97l-8.415 8.385v3h3l8.385-8.415z"/></svg>`;
   const checkSvg = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10-10"/></svg>`;
   const spinnerSvg = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-loader-2 pyodide-spinner" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3a9 9 0 1 0 9 9"/></svg>`;
 
@@ -82,7 +82,6 @@ await micropip.install([${escaped}])
   function toggleEditing(pre, editBtn) {
     if (pre.dataset.pyRunnerEditable === "1") {
       disableEditing(pre, editBtn);
-      showSuccess(editBtn, pencilSvg);
     } else {
       enableEditing(pre);
       editBtn.classList.add("active");
